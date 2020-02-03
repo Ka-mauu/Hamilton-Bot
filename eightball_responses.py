9976966eab084fa6ec1e2a2bd378a423eddc6a46
lerrific <lerrific@gmail.com>
2020-02-04 09:46:27 +1100

    .

--- eightball_responses.py
+++ eightball_responses.py
@@ -19,7 +19,7 @@ def responses(question: str):
         answer = ['Ok, you definitely show the symptoms, so yes.', 'Ok. you do not show the symptoms, fortunately.', 'Ok, the universe has decided so.',
                   'Ok... Rest assured, because you are not.', 'Ok... Rest assured, because you are.', 'Ok... Regardless of what you feel you are, just know that you are loved.', 'Ok, I raise you a better question... Are you stupid?']
 
-    if q.startswith('is') or q.startswith('does') or q.startswith('do i') or q.startswith('do we') or q.startswith('did') or q.startswith('have i') or q.startswith('have you') or q.startswith('have we'):
+    if q.startswith('is') or q.startswith('does') or q.startswith('do i') or q.startswith('do we') or q.startswith('did') or q.startswith('have'):
 
         answer = ['Ok, I can say yes without a doubt.', 'Ok, I can say no without a doubt.', 'Ok... Well, no, unfortunately.', 'Ok... Well, yes, unfortunately.', 'Ok, I raise you a better question... Are you stupid?',
                   'Ok, the universe has decided so.', 'Ok, I say this with utmost certainty... Yes.', 'Ok, I say this with utmost certainty... No.', 'Ok, I do not think so.', 'Ok, I do believe so.']
