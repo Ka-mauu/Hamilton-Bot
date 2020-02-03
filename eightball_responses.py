eff25e9db05fd8bc6093e4a589c253364febfb3e
KarmaKam <60584787+KarmaKam@users.noreply.github.com>
2020-02-02 14:45:01 -1000

    Update eightball_responses.py

--- eightball_responses.py
+++ eightball_responses.py
@@ -9,21 +9,21 @@ def responses(question: str):
 
     if question.lower().startswith('should') or question.lower().startswith('can'):
 
-        answer = ['Ok, I have no opinion on any of your trivial questions. Next!', 'Ok, yeah. That sounds good.', 'Ok, no. Although I really do pity you.', 'Ok, but venture only if you dare...', 'Ok, but do not come crying to me when you inevitably regret it.', 'Ok, I cannot believe how unfathomably dense you are. Goodbye.',
+        answer = ['Ok, yeah. That sounds good.', 'Ok, no. Although I really do pity you.', 'Ok, but venture only if you dare...', 'Ok, but do not come crying to me when you inevitably regret it.', 'Ok, I cannot believe how unfathomably dense you are. Goodbye.',
                   'Ok. That is a batshit crazy notion, but I am all for it.', 'Ok, but why are you asking me this?', 'Ok, I honestly could not care less about what you want to do.', 'Ok, do whatever you feel you must do. I shall not judge.', 'Ok, the universe has decided so.']
 
     if question.lower().startswith('am'):
 
-        answer = ['Ok, I have no opinion on any of your trivial questions. Next!', 'Ok, you definitely show the symptoms, so yes.', 'Ok. you do not show the symptoms, fortunately.', 'Ok, the universe has decided so.',
+        answer = ['Ok, you definitely show the symptoms, so yes.', 'Ok. you do not show the symptoms, fortunately.', 'Ok, the universe has decided so.',
                   'Ok... Rest assured, because you are not.', 'Ok... Rest assured, because you are.', 'Ok... Regardless of what you feel you are, just know that you are loved.', 'Ok, I raise you a better question... Are you stupid?']
 
-    if question.lower().startswith('is') or question.lower().startswith('does') or question.lower().startswith('do i') or question.lower().startswith('do we') or question.lower().startswith('did it'):
+    if question.lower().startswith('is') or question.lower().startswith('does') or question.lower().startswith('do i') or question.lower().startswith('do we') or question.lower().startswith('did'):
 
-        answer = ['Ok, I have no opinion on any of your trivial questions. Next!', 'Ok, I can say yes without a doubt.', 'Ok, I can say no without a doubt.', 'Ok... Well, no, unfortunately.', 'Ok... Well, yes, unfortunately.', 'Ok, I raise you a better question... Are you stupid?', 'Ok, the universe has decided so.', 'Ok, I say this with the utmost certainty... Yes.', 'Ok, I say this with the utmost certainty... No.']
+        answer = ['Ok, I can say yes without a doubt.', 'Ok, I can say no without a doubt.', 'Ok... Well, no, unfortunately.', 'Ok... Well, yes, unfortunately.', 'Ok, I raise you a better question... Are you stupid?', 'Ok, the universe has decided so.', 'Ok, I say this with the utmost certainty... Yes.', 'Ok, I say this with the utmost certainty... No.']
 
     if question.lower().startswith('if') or question.lower().startswith('will') or question.lower().startswith('would') or question.lower().startswith('could') or question.lower().startswith('shall'):
 
-        answer = ['Ok, I have no opinion on any of your trivial questions. Next!', 'Ok, no. Although I really do pity you.', 'Ok, as I see it, you will. Just be cautious.', 'Ok, with all things considered, yes.', 'Ok, I cannot be certain, but I wish you luck.', 'Ok. Fortunately, no.',
+        answer = ['Ok, no. Although I really do pity you.', 'Ok, as I see it, you will. Just be cautious.', 'Ok, with all things considered, yes.', 'Ok, I cannot be certain, but I wish you luck.', 'Ok. Fortunately, no.',
                   'Ok, I do not think I can answer this for you.', 'Ok, I do not think you ever will, and it is not wise to try.', 'Ok, you are better off not doing that. I would like to see you try, though.', 'Ok, the universe has decided so.', 'Ok, do whatever you feel you must do. I shall not judge.']
 
     # custom responses
